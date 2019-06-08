a50b69bac25a2f0b9e8496058fc32cf44a3608f6
Little Milo <no-reply>
2019-06-08 15:18:08 -0700

    added schema and seeds with knex

---
+++ models/knexfile.js
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+module.exports = {
+  client: 'mysql',
+  connection: {
+    host: 'localhost',
+    port: 3306,
+    // change user depending on USER.
+    user: 'nodeUser',
+    password: '',
+    database: 'pets_DB',
+    debug: ['ComQueryPacket', 'RowDataPacket']
+  }
+}
