3119012c4441c10f233b51f5bed39e9028e72931
Little Milo <no-reply>
2019-06-07 17:25:28 -0700

    working on linter issues, one error left, pseudo code for example.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,14 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 // Dependencies
+const ENV = process.env.NODE_ENV || 'development'
 
 /* eslint-disable  no-unused-vars */
-// const mysql = require('mysql')
+const mysql = require('mysql')
 /* eslint-enable  no-unused-vars */
 
 // Creates mySQL connection using Knex.js
-// const Knex = require('knex')(require('../knexfile')[ENV]) //TODO: fix ENV error
+const Knex = require('knex')(require('../knexfile')[ENV])
 
 // Exports the connection for other files to use
-// module.exports = Knex
+module.exports = Knex
--- models/example.js
+++ models/example.js
@@ -1,39 +1,58 @@
 // *********************************************************************************
 // Example.js - This file offers a set of easier-to-use methods for interacting with the MySQL db.
 // *********************************************************************************
+//  database name petinder_db
+/*
+TODO:
 
+Data to be save:
+* Pet name varchar
+* Pet type varchar
+* picuter path varchar
+* address of the shelter ??????
+* answers as aray of numbers ??????
+*/
+//
+//
+//
+//
 // Dependencies
 // =============================================================
 const knex = require('../config/connection.js')
 
 /**
- * Queries the Example database
- *
- * @class Example
- */
+* Queries the Example database
+*
+* @class Example
+*/
+
+// FIXME: change class name
+
 class Example {
+// FIXME: cahnge table name
   constructor (table = 'example') {
     this.table = table
   }
 
   /**
-   *
-   * Find all Examples in the table
-   * @returns Promise
-   * @memberof Example
-   */
+  *
+  * Find all Examples in the table
+  * @returns Promise
+  * @memberof Example
+  */
   findAll () {
     return knex.select()
       .table(this.table)
   }
 
   /**
- * create a new record
- *
- * @param {Object} values The values to insert in the form of {column: value}
- * @returns Promise
- * @memberof Example
- */
+* create a new record
+*
+* @param {Object} values The values to insert in the form of {column: value}
+* @returns Promise
+* @memberof Example
+*/
+  // TODO: imprement this to save the pet data to mysql
   create (values) {
     return knex(this.table)
       .returning('id')
@@ -41,16 +60,36 @@ class Example {
   }
 
   /**
-   * delete 1 or more records by criteria
-   *
-   * @param {Object} where The where clause in the form of {column: value}
-   * @returns Promise
-   * @memberof Example
-   */
+  * to get the pet matched with user
+  * @method Adriano
+  * @param {Object} values The values to insert in the form of {column: value}
+  * @returns Promise
+  */
+  findWhere (values) {
+    console.log(values) // THIS IS JUST TO VOID LINTER ERROR
+  }
+
+  /**
+  * delete 1 or more records by criteria
+  *
+  * @param {Object} where The where clause in the form of {column: value}
+  * @returns Promise
+  * @memberof Example
+  */
+  // ????????
   destroy (where) {
     return knex(this.table)
       .where(where)
       .del()
+  }
+  /**
+*
+*@param {*} a
+*@param {*} b
+*@param {*} c
+*/
+  testFunc (a, b, c) {
+    console.log(`${a}${b}${c}`)
   }
 }
 
