c80b55d578aafdb6df7a161eafae3c2197d3e7a7
Little Milo <no-reply>
2019-06-08 23:06:18 -0700

    defined ENV in connection.js, no linter errors anymore

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,13 @@
 const mysql = require('mysql')
 /* eslint-enable  no-unused-vars */
 
+// needed for heroku
+const ENV = process.env.NODE_ENV || 'development'
+
 // Creates mySQL connection using Knex.js
 const Knex = require('knex')(require('../models/knexfile')[ENV])
-/* eslint-enable no-undef */
+
+/* eslint-enable, no-undef */
 
 // Exports the connection for other files to use
 module.exports = Knex
