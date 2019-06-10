375ad348e756fcf8ab5160ef075e60df01b74225
Little Milo <no-reply>
2019-06-09 19:00:19 -0700

    fourthTry

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,39 @@
 // Update with your config settings.
 
 module.exports = {
+
   development: {
     client: 'mysql',
     connection: {
       host: 'localhost',
       port: 3306,
-      // change user depending on USER.
       user: 'nodeUser',
       password: '',
       database: 'pets_DB',
       debug: ['ComQueryPacket', 'RowDataPacket']
+    }
+  },
+
+  staging: {
+    client: 'mysql',
+    connection: {
+      host: 'localhost',
+      port: 3306,
+      user: 'nodeUser',
+      password: '',
+      database: 'pets_DB',
+      debug: ['ComQueryPacket', 'RowDataPacket']
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DATABASE_URL,
+      port: 3306,
+      user: 'nodeUser',
+      password: '',
+      database: 'pets_DB'
     }
   }
 }
