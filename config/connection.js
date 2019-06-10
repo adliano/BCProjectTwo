99ff03249af0a8b96c16d99a6ea89da90d89ea04
Little Milo <no-reply>
2019-06-09 17:38:56 -0700

    secondTest

--- config/connection.js
+++ config/connection.js
@@ -2,16 +2,17 @@
 // CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
 // *********************************************************************************
 // Dependencies
+// needed for heroku
+const ENV = process.env.NODE_ENV || 'development'
 
 /* eslint-disable  no-unused-vars, no-undef */
 const mysql = require('mysql')
 /* eslint-enable  no-unused-vars */
 
-// needed for heroku
-// const ENV = process.env.NODE_ENV || 'development'
-
 // Creates mySQL connection using Knex.js
 const Knex = require('knex')(require('../models/knexfile')[ENV])
+// const knex = require('knex')('production')
+
 // const Knex = require('knex')(require('../models/knexfile'))
 
 /* eslint-enable, no-undef */
