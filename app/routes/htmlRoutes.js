e73f276b9ffecf64818b91238cb09086be629537
Adliano Alves <adliano@me.com>
2019-06-10 18:55:38 -0700

    change route and index.html modified:   app/public/index.html modified:   app/routes/htmlRoutes.js

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -10,15 +10,15 @@ const path = require('path')
  */
 module.exports = function (app) {
   // Load index page
-  app.get('/', function (req, res) {
-    Example.findAll({})
-      .then(function (dbExamples) {
-        res.render('index', {
-          msg: 'Welcome!',
-          examples: dbExamples
-        })
-      })
-  })
+  // app.get('/', function (req, res) {
+  //   Example.findAll({})
+  //     .then(function (dbExamples) {
+  //       res.render('index', {
+  //         msg: 'Welcome!',
+  //         examples: dbExamples
+  //       })
+  //     })
+  // })
 
   // Load example page and pass in an example by id
   app.get('/find/:id', function (req, res) {
@@ -30,7 +30,10 @@ module.exports = function (app) {
       })
   })
 
-  // findPet
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+  })
+  // addPet
   app.get('/addPet', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/addPet.html'))
   })
