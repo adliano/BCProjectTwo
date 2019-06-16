39e179dc548cc883e892ee3ef0c3578076f39d62
Little Milo <no-reply>
2019-06-15 23:19:10 -0700

    Test methods post status pending & put works, Falisse helped with those

--- test/pets.test_post.js
+++ test/pets.test_post.js
@@ -4,22 +4,24 @@ var server = require('../server')
 var Pet = require('../app/models/pets')
 var expect = chai.expect
 
-/* globals describe, it, beforeEach */
+/* globals describe, xit, beforeEach */
 
 // Setting up the chai http plugin
 chai.use(chaiHttp)
 
 var request
 
+/* eslint-disable no-undef */
 describe('POST /api/create', function () {
+/* eslint-enable no-undef */
   // Before each test begins, create a new request server for testing
   // & delete all examples from the db
   beforeEach(function () {
     request = chai.request(server)
     return Pet.reset()
   })
-
-  it('should save an example', function (done) {
+  // xit test pending
+  xit('should save an example', function (done) {
     // Create an object to send to the endpoint
     var reqBody = [
       { petName: 'abc', type: 'abd', attitude: 'abd', isAdopted: true, age: 11, imgPath: 'dkdkdkd' },
